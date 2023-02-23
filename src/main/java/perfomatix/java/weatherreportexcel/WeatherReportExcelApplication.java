083ecd167608361d0575d8cbc20d2db03b7533e3
UtharaPUnni <uthara.unni@perfomatix.com>
2023-02-23 13:34:16 +0530

    firstcommit-weather-report

---
+++ src/main/java/perfomatix/java/weatherreportexcel/WeatherReportExcelApplication.java
@@ -0,0 +1,13 @@
+package perfomatix.java.weatherreportexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherReportExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherReportExcelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/perfomatix/java/weatherreportexcel/WeatherReportExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package perfomatix.java.weatherreportexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherReportExcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
